a0ec05c95cac2d9d7fb1fe62e7932fa9e4f9220e
Richie Zeng <richzeng@gmail.com>
2012-04-12 11:38:47 -0700

    updated the pvcam project and header files

--- pvcam_dlm/pvcam_dlm.h
+++ pvcam_dlm/pvcam_dlm.h
@@ -25,6 +25,10 @@ by Bob Gunion in 2008.
 #define ARRLEN(arr) (sizeof(arr)/sizeof(arr[0]))
 
 extern "C" {
+/*****************************************************************************
+  pvcam_init.cpp
+*****************************************************************************/
+
   /* pvcam_init(short *hcam); */
   void pvcam_init(int argc, IDL_VPTR argv[], char *argk);
   
@@ -44,7 +48,12 @@ extern "C" {
                  unsigned short pbin,
                  unsigned long *stream_size); */
   void pvcam_setup(int argc, IDL_VPTR argv[], char *argk);
-  
+
+
+/*****************************************************************************
+  pvcam_exposure.cpp
+*****************************************************************************/
+
   /* pvcam_exp_start(short hcam, unsigned short* image); */
   void pvcam_exp_start(int argc, IDL_VPTR argv[], char *argk);
   
@@ -83,19 +92,29 @@ extern "C" {
   
   /* pvcam_set_clear_cycles(short hcam, unsigned short clear_cycles); */
   void pvcam_set_clear_cycles(int argc, IDL_VPTR argv[], char *argk);
-  
+
+  /* pvcam_get_frame_capable(short hcam, unsigned short *frame_capable); */
+  IDL_VPTR pvcam_get_frame_capable(int argc, IDL_VPTR argv[], char *argk);
+
+
+/*****************************************************************************
+  pvcam_temperature.cpp
+*****************************************************************************/
+
   /* pvcam_get_tmp(short hcam, float *cur_tmp); */
   IDL_VPTR pvcam_get_tmp(int argc, IDL_VPTR argv[], char *argk);
-  
-  /* pvcam_init(short *hcam); */
-  void pvcam_set_tmp(int argc, IDL_VPTR argv[], char *argk);
   
   /* pvcam_get_tmp_setpoint(short hcam, float *tmp_setpoint); */
   IDL_VPTR pvcam_get_tmp_setpoint(int argc, IDL_VPTR argv[], char *argk);
   
   /* pvcam_set_tmp_setpoint(short hcam, float tmp_setpoint); */
   void pvcam_set_tmp_setpoint(int argc, IDL_VPTR argv[], char *argk);
-  
+ 
+
+/*****************************************************************************
+  pvcam_gain.cpp
+*****************************************************************************/
+
   /* pvcam_get_max_gain(short hcam, short *max_gain); */
   IDL_VPTR pvcam_get_max_gain(int argc, IDL_VPTR argv[], char *argk);
   
@@ -104,7 +123,12 @@ extern "C" {
   
   /* pvcam_set_gain(short hcam, short gain); */
   void pvcam_set_gain(int argc, IDL_VPTR argv[], char *argk);
-  
+
+
+/*****************************************************************************
+  pvcam_speed.cpp
+*****************************************************************************/
+
   /* pvcam_get_speed(short hcam, unsigned short *speed); */
   IDL_VPTR pvcam_get_speed(int argc, IDL_VPTR argv[], char *argk);
   
@@ -119,7 +143,12 @@ extern "C" {
 
   /* pvcam_get_bits(short hcam, short *bits); */
   IDL_VPTR pvcam_get_bits(int argc, IDL_VPTR argv[], char *argk);
-  
+
+
+/*****************************************************************************
+  pvcam_timeout.cpp
+*****************************************************************************/
+
   /* pvcam_get_retries(short hcam, unsigned short *retries); */
   IDL_VPTR pvcam_get_retries(int argc, IDL_VPTR argv[], char *argk);
   
@@ -131,9 +160,6 @@ extern "C" {
   
   /* pvcam_set_timeout(short hcam, unsigned short m_sec); */
   void pvcam_set_timeout(int argc, IDL_VPTR argv[], char *argk);
-  
-  /* pvcam_get_frame_capable(short hcam, unsigned short *frame_capable); */
-  IDL_VPTR pvcam_get_frame_capable(int argc, IDL_VPTR argv[], char *argk);
 }
 
 #endif
